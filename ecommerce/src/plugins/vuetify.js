2679eaa37589351b21a87bd6dd655829de7d101e
jennysol <jennyssoliver@gmail.com>
2022-03-30 08:30:14 -0300

    Feature: Add vuetify config theme

--- ecommerce/src/plugins/vuetify.js
+++ ecommerce/src/plugins/vuetify.js
@@ -1,7 +1,26 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
+import pt from 'vuetify/lib/locale/pt'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  theme: {
+    themes: {
+      light: {
+        primary: '#00519D',
+        secondary: '##707070',
+        third: '#1F41FC',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  },
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  }
+})
