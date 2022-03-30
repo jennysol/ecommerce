a4c17d50b7ab2d666167223824b57155a962dcad
jennysol <jennyssoliver@gmail.com>
2022-03-30 08:30:31 -0300

    Feature: Start home page

---
+++ ecommerce/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1>Home page</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
