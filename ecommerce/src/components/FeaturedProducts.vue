b3d95a731745c0706919307e7c19737fee1a7acb
jennysol <jennyssoliver@gmail.com>
2022-04-01 00:26:35 -0300

    Feature: Add FeatureProducts

---
+++ ecommerce/src/components/FeaturedProducts.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-col cols="12">
+    <v-sheet
+      class="mx-auto"
+    >
+      <v-slide-group multiple>
+        <v-slide-item
+          v-for="product in featuredProducts"
+          :key="product.id"
+        >
+          <v-card height="50%" width="70%" flat>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <v-avatar
+                class="ma-3"
+                tile
+              >
+                <v-img :src="product.image"></v-img>
+              </v-avatar>
+              <div >
+                <v-card-title class="title">${{ product.price }}</v-card-title>
+                <v-card-subtitle class="subtitle">{{ product.title }}</v-card-subtitle>
+                <v-btn color="primary" small>Adicionar</v-btn>
+              </div>
+            </div>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+  </v-col>
+</template>
+
+<script>
+  export default {
+    name: 'FeaturedProducts',
+    data: () => ({
+      featuredProducts: [],
+      model: null
+    }),
+    mounted() {
+      this.getFeaturedProducts()
+    },
+    methods: {
+      getFeaturedProducts() {
+        fetch('https://fakestoreapi.com/products?limit=5')
+          .then(res=> res.json())
+          .then(json=> this.featuredProducts = json)
+      }
+    }
+  }
+</script>
---
+++ ecommerce/src/views/Home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-col cols="12" class="mt-6">
+    <v-row>
+      <v-col cols="12">
+        <v-text-field 
+          v-model="search" 
+          solo 
+          placeholder="Busque seu produto"
+        >
+          <v-icon
+            slot="append"
+            color="primary"
+          >
+            mdi-magnify
+          </v-icon>
+        </v-text-field>
+      </v-col>
+      <FeaturedProducts/>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import FeaturedProducts from '../../components/FeaturedProducts'
+export default {
+  name: 'Home',
+  components: { FeaturedProducts },
+  data: () => ({
+      search: '',
+  }),
+}
+</script>
